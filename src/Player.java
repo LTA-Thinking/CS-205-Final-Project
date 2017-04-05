266e3294bccdcedb0228bdb532c6e940e82eecab
neal <neal@65-183-148-36-static.burlingtontelecom.net>
2017-04-05 12:23:01 -0400

    add pathExists method in player

--- src/Player.java
+++ src/Player.java
@@ -6,41 +6,103 @@ import javafx.geometry.Point2D;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author neal
  */
 public abstract class Player {
-    private Point2D location; 
+
+    private Point2D location;
     private Card[] treasures = new Card[24];
-    
+
     /**
      * set player's location to the input
+     *
      * @param location
      */
-    public Player(Point2D location){
+    public Player(Point2D location) {
         this.location = location;
+    }
+
+    /**
+     *
+     */
+    public abstract void takeTurn();
+
+    /**
+     * take in a location, return true if a path exist from the current location
+     * to the input location, otherwise return false
+     *
+     * @param location
+     * @return
+     */
+    public boolean pathExists(Point2D location){
+        boolean[][] visited = new boolean[7][7];
+        return pathExistsHelper(visited, this.location, location);
     }
     
     /**
-     * 
-     */
-    public abstract void takeTurn();
-    
-    /**
-     * take in a location, return true if a path exist from the current 
-     * location to the input location, otherwise return false
-     * @param location
+     * recursive method help pathExists to find if a path exists from one location 
+     * to another. 
+     * @param visited
+     * @param start
+     * @param end
      * @return 
      */
-    public boolean pathExist(Point2D location){
-       
+    public boolean pathExistsHelper(boolean[][] visited, Point2D start, Point2D end) {
+        int x1 = (int) start.getX();
+        int y1 = (int) start.getY();
+        int x2 = (int) end.getX();
+        int y2 = (int) end.getY();
+
+        visited[x1][y1] = true;
+        if (y1 < 6) {
+            if (!visited[x1][y1 + 1] && board.canMove(start, 'N')) {
+                if (x1 == x2 && y2 == y1 + 1) {
+                    return true;
+                }
+                if (pathExistsHelper(visited, new Point2D(x1, y1), new Point2D(x2, y2))) {
+                    return true;
+                }
+            }
+        }
+        if (y1 > 1) {
+            if (!visited[x1][y1 - 1] && board.canMove(start, 'S')) {
+                if (x1 == x2 && y2 == y1 + 1) {
+                    return true;
+                }
+                if (pathExistsHelper(visited, new Point2D(x1, y1 - 1), new Point2D(x2, y2))) {
+                    return true;
+                }
+            }
+        }
+        if (x1 > 1) {
+            if (!visited[x1][y1 - 1] && board.canMove(start, 'W')) {
+                if (x2 == x1 - 1 && y2 == y1) {
+                    return true;
+                }
+                if (pathExistsHelper(visited, new Point2D(x1 - 1, y1), new Point2D(x2, y2))) {
+                    return true;
+                }
+            }
+        }
+        if (y1 < 6) {
+            if (!visited[x1 + 1][y1] && board.canMove(start, 'E')) {
+                if (x2 == x1 + 1 && y2 == y1) {
+                    return true;
+                }
+                if (pathExistsHelper(visited, new Point2D(x1 + 1, y1), new Point2D(x2, y2))) {
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 
     /**
      * get the location of the player
+     *
      * @return the location
      */
     public Point2D getLocation() {
@@ -49,10 +111,11 @@ public abstract class Player {
 
     /**
      * set the location of the player
+     *
      * @param location the location to set
      */
     public void setLocation(Point2D location) {
         this.location = location;
     }
-    
+
 }
