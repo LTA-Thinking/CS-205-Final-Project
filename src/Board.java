80515c73475556837cff0074304b03b2d09c52bf
Ivan J Spizizen <ivan.spizizen@uvm.edu>
2017-04-05 13:47:59 -0400

    Board Skeleton
    Board methods outline

---
+++ src/Board.java
@@ -0,0 +1,38 @@
+import javafx.scene.layout.Pane;
+
+public class Board extends Pane{
+
+	// can I move from square at x, y one space in the direction specified? 
+	// direction can be specified as NORTH, SOUTH, EAST, or WEST
+	public boolean CanMove(int x, int y, direction){
+	
+	}
+	
+	//insert a tile into a specific coordinate
+	public void InsertTile(int x, int y){
+		
+	}
+	
+	//construct board graphics
+	public void BoardGraphics(Graphics g){
+		
+	}
+	
+	Gets & Sets
+	
+	//gets the piece that the player will use next
+	//set is a public void
+	//get is public (return)
+	
+	
+	//show the last move made
+	public int LastMove(){
+		
+	}
+	
+	//highlight all possible moves
+	public void HighlightMoves(int x, int y){
+		
+	}
+	
+}
